a1883a52602a438547460a436e81dd594906deb1
Emmanue ELOKA <85324899+EmmanuelELOKA@users.noreply.github.com>
2024-09-22 15:55:04 -0600

    more fixes

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,32 @@ module.exports = {
     entry: './src/main/js/app.js',
     devtool: 'sourcemaps',
     cache: true,
-	mode: 'development',
+    mode: 'development',
     resolve: {
         alias: {
             'stompjs': __dirname + '/node_modules' + '/stompjs/lib/stomp.js',
+        },
+        fallback: {
+            net: require.resolve('net-browserify'), // Add this line for the net fallback
+            // You can add other fallbacks if necessary
         }
     },
     output: {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
-	module: {
-		rules: [
-			{
-				test: path.join(__dirname, '.'),
-				exclude: /(node_modules)/,
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						presets: ["@babel/preset-env", "@babel/preset-react"]
-					}
-				}]
-			}
-		]
-	}
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            }
+        ]
+    }
 };
